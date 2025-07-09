2a51b9b31e93c9deb798aece608fc57489e91f71
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-08 21:09:33 -0400

    Lecture_48: moving components into their own file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,6 @@
-import reactImage from './assets/react-core-concepts.png';
 import {CORE_CONCEPTS} from "./data";
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-    return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-    return (
-        <header>
-            <img src={reactImage} alt="Stylized atom" />
-            <h1>React Essentials</h1>
-            <p>
-                {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are going to build!
-            </p>
-        </header>
-    );
-}
-
-function CoreConcept({title, image, description}) {
-    return (
-        <li>
-            <img src={image} alt={title} />
-            <h3>{title}</h3>
-            <p>{description}</p>
-        </li>
-    )
-}
+import Header from "./components/Header";
+import CoreConcept from "./components/CoreConcept";
 
 function App() {
     return (
