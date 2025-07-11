054d00ff220202540b62d94edec07affa1fd56b9
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-10 22:43:32 -0400

    Lecture_58 output array/list dynamicly using map function

--- src/App.jsx
+++ src/App.jsx
@@ -23,14 +23,7 @@ function App() {
             <main>
                 <section id="core-concepts">
                     <ul>
-                        <CoreConcept
-                            title={CORE_CONCEPTS[0].title}
-                            description={CORE_CONCEPTS[0].description}
-                            image={CORE_CONCEPTS[0].image}
-                        />
-                        <CoreConcept {...CORE_CONCEPTS[1]}/>
-                        <CoreConcept {...CORE_CONCEPTS[2]}/>
-                        <CoreConcept {...CORE_CONCEPTS[3]}/>
+                        {CORE_CONCEPTS.map((eachConcept) => <CoreConcept key={eachConcept.title} {...eachConcept}/>)}
                     </ul>
                 </section>
                 <section id="examples">
