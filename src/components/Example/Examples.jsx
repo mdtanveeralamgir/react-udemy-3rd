c3da8c386a2523f1030c4e72ae85e74d06939f56
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-11 23:08:55 -0400

    Lecture_67 multiple children. clean up code

--- src/components/Example/Examples.jsx
+++ src/components/Example/Examples.jsx
@@ -1,7 +1,8 @@
-import TabButton from "./TabButton";
+import TabButton from "../Wrapper/TabButton";
 import {EXAMPLES} from "../../data";
 import {useState} from "react";
 import Section from "../../Section";
+import Tabs from "../Wrapper/Tabs";
 
 export default function Examples() {
     //hooks must be called inside component functions
@@ -14,28 +15,30 @@ export default function Examples() {
         //useState(); //wrong
         setSelectedTopic(clickedButton);
     }
-    return (<Section title="Example" id="examples">
-        <menu>
-            <TabButton onClick={() => handleClick('components')}
-                       isSelected={selectedTopic === 'components'}>Components</TabButton>
-            <TabButton onClick={() => handleClick('jsx')}
-                       isSelected={selectedTopic === 'jsx'}>JSX</TabButton>
-            <TabButton onClick={() => handleClick('props')}
-                       isSelected={selectedTopic === 'props'}>Props</TabButton>
-            <TabButton onClick={() => handleClick('state')}
-                       isSelected={selectedTopic === 'state'}>State</TabButton>
-        </menu>
-        {/*{selectedTopic ? <div id='tab-content'>*/}
-        {/*    <h3>{EXAMPLES[selectedTopic].title}</h3>*/}
-        {/*    <p>{EXAMPLES[selectedTopic].description}</p>*/}
-        {/*    <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>*/}
-        {/*</div> : <p>Please select a topic.</p>}*/}
-        {!selectedTopic && <p>Please select a topic.</p>}
-        {selectedTopic && <div id='tab-content'>
+
+    let tabContent = <p>Please select a topic.</p>;
+    if (selectedTopic) {
+        tabContent = <div id='tab-content'>
             <h3>{EXAMPLES[selectedTopic].title}</h3>
             <p>{EXAMPLES[selectedTopic].description}</p>
             <pre><code>{EXAMPLES[selectedTopic].code}</code></pre>
-        </div>}
+        </div>;
+    }
 
+
+    const tabButtons = <>
+        <TabButton onClick={() => handleClick('components')}
+                   isSelected={selectedTopic === 'components'}>Components</TabButton>
+        <TabButton onClick={() => handleClick('jsx')}
+                   isSelected={selectedTopic === 'jsx'}>JSX</TabButton>
+        <TabButton onClick={() => handleClick('props')}
+                   isSelected={selectedTopic === 'props'}>Props</TabButton>
+        <TabButton onClick={() => handleClick('state')}
+                   isSelected={selectedTopic === 'state'}>State</TabButton>
+    </>
+    return (<Section title="Example" id="examples">
+        <Tabs buttons={tabButtons}>
+            {tabContent}
+        </Tabs>
     </Section>);
 }
