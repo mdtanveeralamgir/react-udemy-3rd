fe665c7cf730a4201dccf18a7405a5c96b2108cc
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-11 23:30:49 -0400

    Lecture_69 default props value

--- src/components/Wrapper/Tabs.jsx
+++ src/components/Wrapper/Tabs.jsx
@@ -1,4 +1,4 @@
-export default function Tabs({children, buttons, buttonContainer}) {
+export default function Tabs({children, buttons, buttonContainer = 'menu'}) {
     //using buttonContainer for container doesn't work. the variable name has to start with upper case char
     //Or the argument name should start with upper case char
     const ButtonContainer = buttonContainer;
