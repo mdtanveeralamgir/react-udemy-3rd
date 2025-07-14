e13ac781126e6e58f45bd4161504028398c95171
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-13 22:09:24 -0400

    Lecture_81 displaying x/0 on the game board when click. updating game board array immutable way.

--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -1,3 +1,5 @@
+import {useState} from "react";
+
 const initialGameBoard = [
     [null, null, null],
     [null, null, null],
@@ -5,12 +7,28 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+    function handleBoardCellClick(rowIndex, colIndex) {
+        const gameBoardCellValue = gameBoard[rowIndex][colIndex];
+        if (!gameBoardCellValue) {
+            setGameBoard(prevState => {
+                //updating array in immutable way so the previous array in memory doesnot get updated before.
+                const newGameBoard = [...prevState.map(row => [...row])];
+                newGameBoard[rowIndex][colIndex] = 'X'
+                return newGameBoard;
+            })
+        }
+    }
+
     return (
         <ol id="game-board">
-            {initialGameBoard.map((row, rowIndex) => (<li key={rowIndex}>
+            {gameBoard.map((row, rowIndex) => (<li key={rowIndex}>
                 <ol>
-                    {row.map((PlayerSymbol, colIndex) => (<li key={colIndex}>
-                        <button>{PlayerSymbol}</button>
+                    {row.map((playerSymbol, colIndex) => (<li key={colIndex}>
+                        <button
+                            onClick={() => handleBoardCellClick(rowIndex, colIndex)}>{playerSymbol}
+                        </button>
                     </li>))}
                 </ol>
             </li>))}
