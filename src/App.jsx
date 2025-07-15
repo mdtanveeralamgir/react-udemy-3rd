26f69bc95e1c8248f56f604f2fa8b1c5d8e2a059
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-14 22:56:50 -0400

    Lecture_91 game won logic

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Log from "./Components/Logs/Log.jsx";
 import {useState} from "react";
 import {WINNING_COMBINATIONS} from "./data/winning-combinations.js";
 
+const initialGameBoard = [[null, null, null], [null, null, null], [null, null, null],];
+
 function deriveActivePlayer(gameTurns) {
     let currentPlayer = 'X';
     if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -16,6 +18,25 @@ function App() {
     // const [activePlayer, setActivePlayer] = useState('X');
     const [gameTurns, setGameTurns] = useState([]);
     const currentPlayer = deriveActivePlayer(gameTurns);
+
+    let gameBoard = initialGameBoard;
+
+    for (const turn of gameTurns) {
+        const {square, player} = turn; //array destructuring
+        const {row, col} = square; //object destructuring
+        gameBoard[row][col] = player;
+    }
+    let winner = null;
+    for (const combination of WINNING_COMBINATIONS) {
+        const firstCombination = gameBoard[combination[0].row][combination[0].column];
+        const secondCombination = gameBoard[combination[1].row][combination[1].column];
+        const thirdCombination = gameBoard[combination[2].row][combination[2].column];
+
+        if (firstCombination && firstCombination === secondCombination && firstCombination === thirdCombination) {
+            winner = firstCombination;
+            break;
+        }
+    }
 
     function handleGameBoardCellClick(rowIndex, colIndex) {
         // setActivePlayer(prevState => prevState === 'X' ? 'O' : 'X');
@@ -36,7 +57,8 @@ function App() {
                 <Player initialPlayerName="Player 1" playerSymbol="X" isActive={currentPlayer === 'X'}/>
                 <Player initialPlayerName="Player 2" playerSymbol="O" isActive={currentPlayer === 'O'}/>
             </ol>
-            <GameBoard onGameBoardCellClick={handleGameBoardCellClick} turns={gameTurns}/>
+            {winner && <p>You won! {winner}</p>}
+            <GameBoard gameBoard={gameBoard} onGameBoardCellClick={handleGameBoardCellClick}/>
         </div>
         <Log turns={gameTurns}/>
     </main>
--- src/Components/GameBoard/GameBoard.jsx
+++ src/Components/GameBoard/GameBoard.jsx
@@ -1,32 +1,4 @@
-const initialGameBoard = [[null, null, null], [null, null, null], [null, null, null],];
-
-export default function GameBoard({onGameBoardCellClick, turns}) {
-
-    //we are not managing state here instead using state from different component
-    //it's called deriving state
-    let gameBoard = initialGameBoard;
-
-    for (const turn of turns) {
-        const {square, player} = turn; //array destructuring
-        const {row, col} = square; //object destructuring
-        gameBoard[row][col] = player;
-    }
-
-    // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-    //
-    // function handleBoardCellClick(rowIndex, colIndex) {
-    //     const gameBoardCellValue = gameBoard[rowIndex][colIndex];
-    //     if (!gameBoardCellValue) {
-    //         setGameBoard(prevState => {
-    //             //updating array in immutable way so the previous array in memory doesnot get updated before.
-    //             const newGameBoard = [...prevState.map(row => [...row])];
-    //             newGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-    //             return newGameBoard;
-    //         })
-    //     }
-    //     onGameBoardCellClick();
-    // }
-
+export default function GameBoard({onGameBoardCellClick, gameBoard}) {
 
     return (<ol id="game-board">
         {gameBoard.map((row, rowIndex) => (<li key={rowIndex}>
