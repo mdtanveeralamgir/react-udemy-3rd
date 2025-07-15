4cc117f3017cee00e9f4cba690ba27e9fc90a8a1
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-14 22:25:12 -0400

    Lecture_87* reducing state. less state better.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,24 @@ import GameBoard from "./Components/GameBoard/GameBoard.jsx";
 import Log from "./Components/Logs/Log.jsx";
 import {useState} from "react";
 
+
+function deriveActivePlayer(gameTurns) {
+    let currentPlayer = 'X';
+    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
+        currentPlayer = 'O';
+    }
+    return currentPlayer;
+}
+
 function App() {
-    const [activePlayer, setActivePlayer] = useState('X');
+    // const [activePlayer, setActivePlayer] = useState('X');
     const [gameTurns, setGameTurns] = useState([]);
+    const currentPlayer = deriveActivePlayer(gameTurns);
 
     function handleGameBoardCellClick(rowIndex, colIndex) {
-        setActivePlayer(prevState => prevState === 'X' ? 'O' : 'X');
+        // setActivePlayer(prevState => prevState === 'X' ? 'O' : 'X');
         setGameTurns(prevState => {
-            let currentPlayer = 'X';
-            if (prevState.length > 0 && prevState[0].player === 'X') {
-                currentPlayer = 'O';
-            }
+            const currentPlayer = deriveActivePlayer(prevState);
             return [
                 {square: {row: rowIndex, col: colIndex}, player: currentPlayer}
                 //adding another useState: activePlayer is not a good idea because of
@@ -26,8 +33,8 @@ function App() {
     return <main>
         <div id="game-container">
             <ol id="players" className="highlight-player">
-                <Player initialPlayerName="Player 1" playerSymbol="X" isActive={activePlayer === 'X'}/>
-                <Player initialPlayerName="Player 2" playerSymbol="O" isActive={activePlayer === 'O'}/>
+                <Player initialPlayerName="Player 1" playerSymbol="X" isActive={currentPlayer === 'X'}/>
+                <Player initialPlayerName="Player 2" playerSymbol="O" isActive={currentPlayer === 'O'}/>
             </ol>
             <GameBoard onGameBoardCellClick={handleGameBoardCellClick} turns={gameTurns}/>
         </div>
