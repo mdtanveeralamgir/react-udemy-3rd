29d6fc89646f7524f1cb3baa77c80ae61fd031cb
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-15 22:52:11 -0400

    Lecture_own splitting  functions and const variables in a separate js file for all.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,57 +2,16 @@ import Player from "./Components/Players/Player.jsx";
 import GameBoard from "./Components/GameBoard/GameBoard.jsx";
 import Log from "./Components/Logs/Log.jsx";
 import {useState} from "react";
-import {WINNING_COMBINATIONS} from "./data/winning-combinations.js";
 import GameOver from "./Components/GameOver/GameOver.jsx";
-
-const INITIAL_GAME_BOARD = [[null, null, null], [null, null, null], [null, null, null],];
-const PLAYERS ={
-    X: 'Player 1',
-    O: 'Player 2'
-}
-
-function deriveActivePlayer(gameTurns) {
-    let currentPlayer = 'X';
-    if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
-        currentPlayer = 'O';
-    }
-    return currentPlayer;
-}
-
-function deriveWinner(gameBoard, playerName) {
-    let winner = null;
-    for (const combination of WINNING_COMBINATIONS) {
-        const firstCombination = gameBoard[combination[0].row][combination[0].column];
-        const secondCombination = gameBoard[combination[1].row][combination[1].column];
-        const thirdCombination = gameBoard[combination[2].row][combination[2].column];
-
-        if (firstCombination && firstCombination === secondCombination && firstCombination === thirdCombination) {
-            winner = playerName[firstCombination];
-            break;
-        }
-    }
-
-    return winner;
-}
-
-function deriveGameBoard(gameTurns) {
-    let gameBoard = [...INITIAL_GAME_BOARD.map(row => [...row])];
-
-    for (const turn of gameTurns) {
-        const {square, player} = turn; //array destructuring
-        const {row, col} = square; //object destructuring
-        gameBoard[row][col] = player;
-    }
-
-    return gameBoard;
-}
+import {deriveActivePlayer, deriveWinner, deriveGameBoard} from "./Functions/App.js";
+import {INITIAL_GAME_BOARD, PLAYERS} from "./Variables/App.js";
 
 function App() {
     const [playerName, setPlayerName] = useState(PLAYERS);
     const [gameTurns, setGameTurns] = useState([]);
     const currentPlayer = deriveActivePlayer(gameTurns);
     const hasDraw = gameTurns.length === 9 && !winner;
-    const gameBoard = deriveGameBoard(gameTurns);
+    const gameBoard = deriveGameBoard(INITIAL_GAME_BOARD, gameTurns);
 
     function handleGameBoardCellClick(rowIndex, colIndex) {
         // setActivePlayer(prevState => prevState === 'X' ? 'O' : 'X');
