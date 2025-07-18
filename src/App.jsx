ce1a4d9928cb3633a413a12247801ed0ad176823
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-17 23:00:19 -0400

    Lecture_100* 4 user inputs with single useState and single handle function

--- src/App.jsx
+++ src/App.jsx
@@ -1,93 +1,12 @@
 import Header from "./Component/Header/Header.jsx";
 import UserInput from "./Component/UserInput/UserInput.jsx";
-import ResultTable from "./Component/ResultTable/ResultTable.jsx";
-import {useState} from "react";
-import {calculateInvestmentResults} from "./util/investment";
-import Input from "./Component/UserInput/Input.jsx";
 
 function App() {
-    const [initialInvestmentInput, setInitialInvestmentInput] = useState('');
-    const [annualInvestment, setAnnualInvestment] = useState('');
-    const [expectedReturn, setExpectedReturn] = useState('');
-    const [duration, setDuration] = useState('');
-    const [calculatedResults, setCalculatedResults] = useState([]);
-
-
-    function handleInitialInvestmentInput(e) {
-        setInitialInvestmentInput(+e.target.value);
-        if (+e.target.value && annualInvestment && expectedReturn && duration) {
-            setCalculatedResults(
-                calculateInvestmentResults({
-                    initialInvestment: +e.target.value,
-                    annualInvestment,
-                    expectedReturn,
-                    duration
-                })
-            )
-        }
-    }
-
-    function handleAnnualInvestmentInput(e) {
-        setAnnualInvestment(+e.target.value);
-        if (+e.target.value && initialInvestmentInput && expectedReturn && duration) {
-            setCalculatedResults(
-                calculateInvestmentResults({
-                    initialInvestment: initialInvestmentInput,
-                    annualInvestment: +e.target.value,
-                    expectedReturn,
-                    duration
-                })
-            );
-        }
-    }
-
-    function handleExpectedReturnInput(e) {
-        setExpectedReturn(+e.target.value);
-        if (+e.target.value && initialInvestmentInput && annualInvestment && duration) {
-
-            setCalculatedResults(calculateInvestmentResults({
-                initialInvestment: initialInvestmentInput,
-                annualInvestment,
-                expectedReturn: +e.target.value,
-                duration
-            }));
-
-        }
-    }
-
-    function handleDurationInput(e) {
-        setDuration(+e.target.value);
-        if (+e.target.value && initialInvestmentInput && annualInvestment && expectedReturn) {
-            setCalculatedResults(calculateInvestmentResults({
-                initialInvestment: initialInvestmentInput,
-                annualInvestment,
-                expectedReturn,
-                duration: +e.target.value,
-            }));
-        }
-
-    }
-
-
-    const initialInvestment_input = <Input val={initialInvestmentInput} onInputChange={handleInitialInvestmentInput}
-                                           labelText="Initial Investment"></Input>
-    const annualInvestment_input = <Input val={annualInvestment} onInputChange={handleAnnualInvestmentInput}
-                                          labelText="Annual Investment"></Input>
-    const expectedReturn_input = <Input val={expectedReturn} onInputChange={handleExpectedReturnInput}
-                                        labelText="Expected Return"></Input>
-    const duration_input = <Input val={duration} onInputChange={handleDurationInput}
-                                  labelText="Duration"></Input>
 
     return (
         <>
             <Header/>
-            <UserInput
-                investmentInput={initialInvestment_input}
-                annualInvestment={annualInvestment_input}
-                expectedReturn={expectedReturn_input}
-                durationInput={duration_input}
-            />
-            {calculatedResults.length > 0 && <ResultTable calculated_result={calculatedResults}/>}
+            <UserInput/>
         </>
     )
 }
--- src/Component/UserInput/UserInput.jsx
+++ src/Component/UserInput/UserInput.jsx
@@ -1,16 +1,45 @@
 import './UserInputCss.css'
-import Input from "./Input.jsx";
+import {useState} from "react";
 
-export default function UserInput({investmentInput, annualInvestment,expectedReturn,durationInput}) {
+export default function UserInput() {
+    const [userInputs, setUserInputs] = useState({
+        initialInvestment: 10000,
+        annualInvestment: 1000,
+        expectedReturn: 6,
+        duration: 10,
+    });
+
+    function handleChange(inputIdentifier, newValue) {
+        setUserInputs((prevState) => {
+            return {
+                ...prevState,
+                [inputIdentifier]: newValue
+            }
+        });
+    }
+
     return (
         <section id="user-input">
             <div className="input-group">
-                {investmentInput}
-                {annualInvestment}
+                <p>
+                    <label>Initial Investment</label>
+                    <input value={userInputs.initialInvestment} onChange={(event) => handleChange('initialInvestment', event.target.value)} type="number"
+                           required/>
+                </p>
+                <p>
+                    <label>Annual Investment</label>
+                    <input value={userInputs.annualInvestment} onChange={(event) => handleChange('annualInvestment', event.target.value)} type="number" required/>
+                </p>
             </div>
             <div className="input-group">
-                {expectedReturn}
-                {durationInput}
+                <p>
+                    <label>Expected Return</label>
+                    <input value={userInputs.expectedReturn} onChange={(event) => handleChange('expectedReturn', event.target.value)} type="number" required/>
+                </p>
+                <p>
+                    <label>Duration</label>
+                    <input value={userInputs.duration} onChange={(event) => handleChange('duration', event.target.value)} type="number" required/>
+                </p>
             </div>
         </section>
     )
