932d8670013938bfc6a3fd729660c31ca40c27e5
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-19 00:07:01 -0400

    Lecture_109 how to apply inline styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,12 @@ export default function Header() {
     <header>
       <img src={logo} alt="A canvas" />
       <h1>ReactArt</h1>
+      {/*  inline styling accepts an object.*/}
+      <h2 style={{
+          color: 'red',
+          textAlign: 'left',
+          'text-align': 'left'
+      }}>ReactArt</h2>
       <p>A community of artists and art-lovers.</p>
     </header>
   );
