339a8074476735beba48aa37d082e0d11dbcb1d2
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-19 23:19:44 -0400

    Lecture_130 react developer tool plugin

--- src/index.jsx
+++ src/index.jsx
@@ -6,4 +6,7 @@ import './index.css';
 
 //Strict mode executes each component twice to cath the error
 //It does it only in dev mode. not in prod
+
+//React developer tools plugin for chrome helps understand the components chain
+//details about each component
 ReactDOM.createRoot(document.getElementById('root')).render(<StrictMode><App /></StrictMode>);
