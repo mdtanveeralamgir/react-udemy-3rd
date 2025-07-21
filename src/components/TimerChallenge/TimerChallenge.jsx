1d2865552aa1ce08f021c0a9283a6ef332dfbf76
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-20 22:49:28 -0400

    Lecture_140 adding dialog modal using html dialog element

--- src/components/TimerChallenge/TimerChallenge.jsx
+++ src/components/TimerChallenge/TimerChallenge.jsx
@@ -1,30 +1,13 @@
 import {useState, useRef} from "react";
-
-//let timer;
-/*
-* using this the stop button with work, but if we start 2 different timeChallenge components
-* then we will not be able to stop any of them. Because the timer variable will be replaced by the pointer of 2nd
-* started component.
-* */
+import ResultModal from "../ResultModal/ResultModal.jsx";
 
 export default function TimerChallenge({title, targetTime}) {
 
     const [timerStarted, setTimerStarted] = useState(false);
     const [timerExperied, setTimerExpired] = useState(false);
-    /*
-    * now this timer ref will be tied to each component
-    * each instance of this component will have it's own timer variable that points to
-    * it's own setTimeout pointer
-    * */
-    const timer = useRef();
-    /*
-    * with this solution, the timer cannot be stopped once it's started.
-    *  because when we click the stop button the timerChallenge component
-    * gets recreated and the timer variable that holds the pointer of setTimeout
-    * gets re-assigned to a new pointer of a new setTimeout
-    * */
 
-    //let timer;
+    const timer = useRef();
+
     function handleStart() {
         setTimerStarted(true);
         timer.current = setTimeout(() => {
@@ -37,19 +20,19 @@ export default function TimerChallenge({title, targetTime}) {
         clearTimeout(timer.current);
     }
 
-    return (
-        <section className="challenge">
-            <h2>{title}</h2>
-            {timerExperied && <p>You lost!</p>}
-            <p className="challenge-time">
-                {targetTime} second{targetTime > 1 ? 's' : ''}
-            </p>
-            <p onClick={timerStarted ? handleStop : handleStart}>
-                <button> {timerStarted ? 'Stop' : 'Start'} challenge</button>
-            </p>
-            <p className={timerStarted ? 'active' : undefined}>
-                {timerStarted ? 'Time is running' : 'Timer Inactive'}
-            </p>
-        </section>
-    )
+    return (<>
+            {timerExperied && <ResultModal result={timerExperied ? 'LOST' : 'WON'} targetTime={targetTime}/>}
+            <section className="challenge">
+                <h2>{title}</h2>
+                <p className="challenge-time">
+                    {targetTime} second{targetTime > 1 ? 's' : ''}
+                </p>
+                <p onClick={timerStarted ? handleStop : handleStart}>
+                    <button> {timerStarted ? 'Stop' : 'Start'} challenge</button>
+                </p>
+                <p className={timerStarted ? 'active' : undefined}>
+                    {timerStarted ? 'Time is running' : 'Timer Inactive'}
+                </p>
+            </section>
+        </>)
 }
