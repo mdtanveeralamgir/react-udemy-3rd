5965b2d5a7a288dc6b682d2b7fc35fc16fca1336
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-20 23:09:51 -0400

    Lecture_142* useImperativeHandle hook, to share any element of a component to outside of that component.

--- src/components/ResultModal/ResultModal.jsx
+++ src/components/ResultModal/ResultModal.jsx
@@ -1,6 +1,16 @@
+import {useImperativeHandle, useRef} from "react";
+
 export default function ResultModal({result, targetTime, ref}) {
+    const dialog = useRef();
+    useImperativeHandle(ref, () => {
+        return {
+            open() {
+                dialog.current.showModal();
+            }
+        }
+    });
     return (
-        <dialog ref={ref} className="result-modal">
+        <dialog ref={dialog} className="result-modal">
             <h2>you {result}</h2>
             <p>The target time was <strong>{targetTime}</strong> seconds</p>
             <p>You stopped the timer with <strong>X seconds left</strong></p>
