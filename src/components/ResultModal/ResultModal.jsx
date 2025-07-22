f08d5511114763876a286a817d1c908def19cd49
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-21 22:07:12 -0400

    Lecture_145 calculatng winning score

--- src/components/ResultModal/ResultModal.jsx
+++ src/components/ResultModal/ResultModal.jsx
@@ -4,6 +4,8 @@ export default function ResultModal({targetTime, ref, remainingTime, resetTime})
     const dialog = useRef();
     const userLost = remainingTime <= 0;
     const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
     useImperativeHandle(ref, () => {
         return {
             open() {
@@ -13,7 +15,8 @@ export default function ResultModal({targetTime, ref, remainingTime, resetTime})
     });
     return (
         <dialog ref={dialog} className="result-modal">
-            <h2>{userLost && 'You Lost'}</h2>
+            {userLost && <h2>You Lost</h2>}
+            {!userLost && <h2>Your score: {score}</h2>}
             <p>The target time was <strong>{targetTime}</strong> seconds</p>
             <p>You stopped the timer with <strong>{formattedRemainingTime}</strong></p>
             <form method="dialog" onSubmit={resetTime}>
