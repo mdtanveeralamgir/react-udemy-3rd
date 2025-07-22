926d618db708574299f7e88767a595951f109b0c
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-21 22:16:28 -0400

    Lecture_147* createPortal to move the modal up in the body element.

--- src/components/ResultModal/ResultModal.jsx
+++ src/components/ResultModal/ResultModal.jsx
@@ -1,4 +1,5 @@
 import {useImperativeHandle, useRef} from "react";
+import {createPortal} from "react-dom";
 
 export default function ResultModal({targetTime, ref, remainingTime, resetTime}) {
     const dialog = useRef();
@@ -13,7 +14,7 @@ export default function ResultModal({targetTime, ref, remainingTime, resetTime})
             }
         }
     });
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={resetTime}>
             {userLost && <h2>You Lost</h2>}
             {!userLost && <h2>Your score: {score}</h2>}
@@ -22,6 +23,7 @@ export default function ResultModal({targetTime, ref, remainingTime, resetTime})
             <form method="dialog" onSubmit={resetTime}>
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
 }
