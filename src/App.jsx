bd94591fcf497db747698d118e74f241d61a4314
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-23 21:58:00 -0400

    Lecture_154 having 1 state for all

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,32 @@
 import ProjectPanel from "./Components/Projects/ProjectPanel.jsx";
 import AddProject from "./Components/Projects/AddProject.jsx";
 import {useState} from "react";
+import NoProjectSelected from "./Components/Projects/NoProjectSelected.jsx";
 
 function App() {
     const [isAddProject, setIsAddProject] = useState(false);
-    function handleAddProjectClick(){
-        setIsAddProject(prev => !prev)
+    const [projectState, setProjectState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjectState(prevState => {
+            return {
+                ...prevState,
+                selectedProjectId: null
+            }
+        })
     }
+
+    let content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+    if (projectState.selectedProjectId === null)
+        content = <AddProject/>;
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectPanel onClick={handleAddProjectClick}/>
-            {isAddProject ? <AddProject/> : <p>No project view</p>}
+            <ProjectPanel onStartAddProject={handleStartAddProject}/>
+            {content}
         </main>
     );
 }
--- src/Components/Projects/ProjectPanel.jsx
+++ src/Components/Projects/ProjectPanel.jsx
@@ -1,12 +1,12 @@
 import AddProjectButton from "../Buttons/AddProjectButton.jsx";
 
-export default function ProjectPanel({onClick}) {
+export default function ProjectPanel({onStartAddProject}) {
     return <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
         <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">Your Project</h2>
         <div>
         </div>
         <ul>
-            <AddProjectButton>+ Add Project</AddProjectButton>
+            <AddProjectButton onClick={onStartAddProject}>+ Add Project</AddProjectButton>
         </ul>
     </aside>;
 }
