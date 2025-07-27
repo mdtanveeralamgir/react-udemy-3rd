2bf952ef5db45222129b1124bd8e59976ca55d86
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-07-26 22:18:59 -0400

    lecture_162(own): adding task component

--- src/Components/Tasks/NewTask.jsx
+++ src/Components/Tasks/NewTask.jsx
@@ -1,6 +1,9 @@
-export default function NewTask() {
+import {useRef} from "react";
+
+export default function NewTask({handleAddTask}) {
+const newTaskValue = useRef();
     return <div className="flex gap-4 items-center">
-        <input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200"/>
-        <button className="text-stone-700 hover:text-stone-950">Add Task</button>
+        <input ref={newTaskValue} type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200"/>
+        <button onClick={() => handleAddTask(newTaskValue.current.value)} className="text-stone-700 hover:text-stone-950">Add Task</button>
     </div>
 }
--- src/Components/Tasks/Tasks.jsx
+++ src/Components/Tasks/Tasks.jsx
@@ -1,12 +1,25 @@
 import NewTask from "./NewTask.jsx";
+import {useState} from "react";
+
+
 
 export default function Tasks() {
+    const [taskList, setTaskList] = useState([]);
+
+    function addTask(task) {
+        setTaskList(prevState => [...prevState, {
+            task: task,
+            id: Math.random()
+        }]);
+    }
     return (
         <section>
             <h2 className="text-2xl text-stone-700 font-bold mb-4">Task</h2>
-            <NewTask/>
-            <p className="text-stone-800 my-4">No task</p>
-            <ul></ul>
+            <NewTask handleAddTask={addTask}/>
+            {taskList.length === 0 && <p className="text-stone-800 my-4">No task</p>}
+            <ul>
+                {taskList.map(task => <li key={task.id}>{task.task}</li>)}
+            </ul>
         </section>
     )
 }
