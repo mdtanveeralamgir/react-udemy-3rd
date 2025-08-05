5035962c5a0badbe9815afc0674ad89acfa6420b
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-08-04 22:35:26 -0400

    lecture_194: possible way to manage question and answer.

---
+++ src/components/Quiz/Quiz.jsx
@@ -0,0 +1,7 @@
+import {useState} from 'react'
+export default function Quiz() {
+const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
+const [userAnswer, setUserAnswer] = useState([])
+
+
+}
