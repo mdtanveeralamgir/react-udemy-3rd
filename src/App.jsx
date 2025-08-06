2d57e3448ff8099d73e4fb99e678983f9b6507d8
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-08-05 21:38:53 -0400

    lecture_195: showing questions and selecting answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Header from "./components/Header/Header.jsx";
+import Quiz from "./components/Quiz/Quiz.jsx";
 
 function App() {
-    return (<Header/>)
+    return (
+        <>
+            <Header/>
+            <Quiz/>
+        </>
+    )
 }
 
 export default App;
--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -1,7 +1,22 @@
 import {useState} from 'react'
+import QUESTIONS from '../../questions.js';
+
 export default function Quiz() {
-const [activeQuestionIndex, setActiveQuestionIndex] = useState(0)
-const [userAnswer, setUserAnswer] = useState([])
+    const [userAnswer, setUserAnswer] = useState([])
+    const aciveQuestionIndex = userAnswer.length;
 
+    function handleSelectAnswer(answer) {
+        setUserAnswer(prev => [...prev, answer])
+    }
 
+    return <div id="quiz">
+        <div id="question">
+            <h2>{QUESTIONS[aciveQuestionIndex].text}</h2>
+            <ul id="answers">
+                {QUESTIONS[aciveQuestionIndex].answers.map((answer) => <li key={answer} className="answer">
+                    <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
+                </li>)}
+            </ul>
+        </div>
+    </div>
 }
