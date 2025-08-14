c63669b6f83ba7e7371ca6a574560f8463aa09a3
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-08-13 22:15:43 -0400

    lecture_232: using useEffect to fetch data

---
+++ README.md
@@ -0,0 +1,4 @@
+=> useEffect to fetch data
+
+232: 
+    - using useEffect to fetch data
--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,12 +1,23 @@
+import {useState, useEffect} from 'react';
 import Places from './Places.jsx';
 
-export default function AvailablePlaces({ onSelectPlace }) {
-  return (
-    <Places
-      title="Available Places"
-      places={[]}
-      fallbackText="No places available."
-      onSelectPlace={onSelectPlace}
-    />
-  );
+export default function AvailablePlaces({onSelectPlace}) {
+    const [availablePlaces, setAvailablePlaces] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/places')
+            .then((response) => response.json())
+            .then((resData) => {
+                setAvailablePlaces(resData.places);
+            });
+    }, []);
+
+    return (
+        <Places
+            title="Available Places"
+            places={availablePlaces}
+            fallbackText="No places available."
+            onSelectPlace={onSelectPlace}
+        />
+    );
 }
