45eb9d6b4a57680383389b4fc10d6ea0b748c70a
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-08-20 21:38:20 -0400

    lecture_261: validate form using builtin validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import Header from './components/Header.jsx';
-// import Signup from './components/Signup.jsx';
 // import StateLogin from './components/StateLogin.jsx';
-// import Signup from './components/Signup.jsx';
-import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
+// import Login from './components/Login.jsx';
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Login />
+        <Signup />
       </main>
     </>
   );
