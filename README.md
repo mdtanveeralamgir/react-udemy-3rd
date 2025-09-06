4c06ba8d9a4ebbe8a36ae83727107ef3ecb44025
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-09-05 22:40:48 -0400

    lecture_333*: saving/updating cart items in firebase

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 => redux with async function
 => redux toolkit with async function
+=> firebase
 
 330*:
     - how NOT to use async function in reducer
 332*:
     - where to put logic, reducer/component
+333*:
+    - saving/updating cart items in firebase
--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import {useSelector} from "react-redux";
+import {useEffect} from "react";
 
 function App() {
     const isCartShown = useSelector(state => state.showCart);
+    const cartItem = useSelector(state => state.items);
+
+    useEffect(() => {
+        fetch('https://react-56141-default-rtdb.firebaseio.com/cart.json',{
+            method: 'PUT',
+            body: JSON.stringify(cartItem)
+        })
+    }, [cartItem]);
     return (
         <Layout>
             {isCartShown && <Cart/>}
