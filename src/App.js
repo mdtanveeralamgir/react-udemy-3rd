17d98fcda60d101df15fca90025975b77ca60d58
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-09-07 23:35:47 -0400

    lecture_343: initial project

--- src/App.js
+++ src/App.js
@@ -1,45 +1,5 @@
-import Cart from './components/Cart/Cart';
-import Layout from './components/Layout/Layout';
-import Products from './components/Shop/Products';
-import {useSelector, useDispatch} from "react-redux";
-import {useEffect} from "react";
-import {sendCartData, fetchCartData} from "./store/cart-actions";
-import Notification from "./components/Notification/Notification";
-
-let initialRender = true;
-
 function App() {
-    const isCartShown = useSelector(state => state.showCart);
-    const cartItem = useSelector(state => state.items);
-    const isCartItemChanged = useSelector(state => state.isChanged);
-    const dispatch = useDispatch();
-    const isNotification = useSelector(state => state.Notification);
-
-    useEffect(() => {
-        dispatch(fetchCartData());
-
-    }, []);
-
-    useEffect(() => {
-
-        if (initialRender) {
-            initialRender = false;
-            return;
-        }
-        if (isCartItemChanged)
-            dispatch(sendCartData(cartItem));
-
-    }, [cartItem, dispatch]);
-    return (
-        <>
-            {isNotification && <Notification status={isNotification.status} title={isNotification.title}
-                                             message={isNotification.message}/>}
-            <Layout>
-                {isCartShown && <Cart/>}
-                <Products/>
-            </Layout>
-        </>
-    );
+  return <div></div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import store from "./store";
 
 import './index.css';
 import App from './App';
-import {Provider} from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Provider store={store}><App /></Provider>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
