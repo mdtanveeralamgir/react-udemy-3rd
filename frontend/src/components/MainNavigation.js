25e9498bc3fa641ee59c46b2df495d7740469b00
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-09-11 23:16:09 -0400

    lecture_own: doing activities in app.js from 1-5

--- frontend/src/components/MainNavigation.js
+++ frontend/src/components/MainNavigation.js
@@ -1,20 +1,31 @@
 import classes from './MainNavigation.module.css';
+import {Outlet} from "react-router-dom";
+import {NavLink} from "react-router-dom";
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <nav>
-        <ul className={classes.list}>
-          <li>
-            <a>Home</a>
-          </li>
-          <li>
-            <a>Events</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+    return (
+        <>
+            <header className={classes.header}>
+                <nav>
+                    <ul className={classes.list}>
+                        <li>
+                            <NavLink to="/"
+                                     className={({isActive}) => isActive ? classes.active : undefined}>Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="events"
+                                     className={({isActive}) => isActive ? classes.active : undefined}>Events</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="events/new" className={({isActive}) => isActive ? classes.active : undefined}>New
+                                Event</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            {<Outlet/>}
+        </>
+    );
 }
 
 export default MainNavigation;
