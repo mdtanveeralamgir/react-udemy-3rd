9485a6b5606fc80f2e7a0d858eed3ec83158bca3
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-09-13 22:33:44 -0400

    lecture_own: adding events in event page and showing ID in the event details page

--- frontend/src/Page/EventDetailpage.js
+++ frontend/src/Page/EventDetailpage.js
@@ -1,3 +1,6 @@
+import {useParams} from "react-router-dom";
+
 export default function EventDetailPage() {
-    return <h1>EventDetailPage Page</h1>;
+    const {id} = useParams();
+    return <h1>{id}</h1>;
 }
--- frontend/src/Page/EventsPage.js
+++ frontend/src/Page/EventsPage.js
@@ -1,3 +1,24 @@
+import {Link} from "react-router-dom";
+
+const EVENTS = [
+    {
+        id: 1,
+        title: "Event 1"
+    },
+    {
+        id: 2,
+        title: "Event 2"
+    },
+    {
+        id: 3,
+        title: "Event 3"
+    },
+]
 export default function EventsPage() {
-    return <h1>Events Page</h1>;
+    return <>
+        <ul>
+            {EVENTS.map(event => <li key={event.id}><Link to={`/events/${event.id}`}>{event.title}</Link></li>)}
+            <li><Link to="events/"></Link></li>
+        </ul>
+    </>
 }
