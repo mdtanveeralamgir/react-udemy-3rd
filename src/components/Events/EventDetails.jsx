bd642a43850ae174874db157fbd5dc9664382bfb
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-02 22:17:17 -0400

    lecture_419: (instructor solution) display event details, delete event using mutation my solution. fix delete bug

--- src/components/Events/EventDetails.jsx
+++ src/components/Events/EventDetails.jsx
@@ -1,58 +1,84 @@
-import {Link, Outlet, useParams} from 'react-router-dom';
+import {Link, Outlet, useParams, useNavigate} from 'react-router-dom';
 import {useQuery, useMutation} from '@tanstack/react-query';
 
 import Header from '../Header.jsx';
-import {deleteEvent, fetchEvent} from "../../util/http.js";
+import {deleteEvent, fetchEvent, queryClient} from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function EventDetails() {
     const {id} = useParams();
+    const navigate = useNavigate();
     const {data, isLoading, isError, error} = useQuery({
-        queryFn: (signal) => fetchEvent({id, signal}),
+        queryKey: ['events', id], queryFn: (signal) => fetchEvent({id, signal}),
     });
 
     const {mutate} = useMutation({
-        mutationFn: deleteEvent({id}),
+        mutationFn: deleteEvent,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: ['events']});
+            navigate('/events');
+        }
     });
 
     function handleDelete() {
-        console.log('delete');
-        mutate();
+        mutate({id});
+    }
+
+    let content;
+    if (isLoading) {
+        content = (<div id="event-details-content" className="center">
+            <p>Fetching event data.......</p>
+        </div>);
+    }
+    if (isError) {
+        content = (<div id="event-details-content" className="center">
+            <ErrorBlock title="Loading failed" message={error.info?.message || 'Failed to load event'}/>
+        </div>);
+    }
+
+    if (data) {
+        const formattedDate = new Date(data.date).toLocaleDateString('en-US', {
+            day: 'numeric',
+            month: 'short',
+            year: 'numeric',
+        })
+
+        content = (
+            <>
+                <header>
+                    <h1>{data.title}</h1>
+                    <nav>
+                        <button onClick={handleDelete}>Delete</button>
+                        <Link to="edit">Edit</Link>
+                    </nav>
+                </header>
+                <div id="event-details-content">
+                    <img src={"http://localhost:3000/" + data.image} alt={data.title}/>
+                    <div id="event-details-info">
+                        <div>
+                            <p id="event-details-location">{data.location}</p>
+                            <time dateTime={`Todo-DateT$Todo-Time`}>{formattedDate}</time>
+                        </div>
+                        <p id="event-details-description">{data.description}</p>
+                    </div>
+                </div>
+            </>
+
+        );
     }
 
 
-    return (
+    return (<>
         <>
-            {isLoading && <p>Loading...</p>}
-            {isError && <p>Failed to load event</p>}
-            {data &&
-                <>
-                    <Outlet/>
-                    <Header>
-                        <Link to="/events" className="nav-item">
-                            View all Events
-                        </Link>
-                    </Header>
-                    <article id="event-details">
-                        <header>
-                            <h1>{data.title}</h1>
-                            <nav>
-                                <button onClick={handleDelete}>Delete</button>
-                                <Link to="edit">Edit</Link>
-                            </nav>
-                        </header>
-                        <div id="event-details-content">
-                            <img src={"http://localhost:3000/" + data.image} alt={data.alt}/>
-                            <div id="event-details-info">
-                                <div>
-                                    <p id="event-details-location">{data.location}</p>
-                                    <time dateTime={`Todo-DateT$Todo-Time`}>{data.date}@{data.time}</time>
-                                </div>
-                                <p id="event-details-description">{data.description}</p>
-                            </div>
-                        </div>
-                    </article>
-                </>
-            }
+            <Outlet/>
+            <Header>
+                <Link to="/events" className="nav-item">
+                    View all Events
+                </Link>
+            </Header>
+            <article id="event-details">
+                {content}
+            </article>
         </>
-    );
+    </>);
 }
