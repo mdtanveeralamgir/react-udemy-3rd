f04dcf5443fdda4c84f310bcecea5a2960ab2268
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-11 22:29:06 -0400

    lecture_443: seperate header background component

--- components/main-header.jsx
+++ components/main-header.jsx
@@ -2,22 +2,26 @@ import Link from "next/link";
 import logo from '@/assets/logo.png';
 import classes from './main-header.module.css';
 import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header-background";
 
 export default function MainHeader() {
-    return <header className={classes.header}>
-        <Link className={classes.logo} href="/">
-            <Image src={logo} alt="logo" priority/>
-            NextLevel Food
-        </Link>
-        <nav className={classes.nav}>
-            <ul>
-                <li>
-                    <Link href="/meals">Browse Meals</Link>
-                </li>
-                <li>
-                    <Link href="/community">Foodies community</Link>
-                </li>
-            </ul>
-        </nav>
-    </header>
+    return <>
+        <MainHeaderBackground/>
+        <header className={classes.header}>
+            <Link className={classes.logo} href="/">
+                <Image src={logo} alt="logo" priority/>
+                NextLevel Food
+            </Link>
+            <nav className={classes.nav}>
+                <ul>
+                    <li>
+                        <Link href="/meals">Browse Meals</Link>
+                    </li>
+                    <li>
+                        <Link href="/community">Foodies community</Link>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    </>
 }
