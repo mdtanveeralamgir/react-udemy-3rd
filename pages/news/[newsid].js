63b3b6540b3ca886f5a3baa640a4c286481ea1d6
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-24 21:33:34 -0400

    lecture_483: initial project

--- pages/news/[newsid].js
+++
@@ -1,8 +0,0 @@
-import {useRouter} from 'next/router';
-
-export default function DetailPage() {
-    const router = useRouter();
-    const {newsid} = router.query;
-
-    return <h1>Detail page {newsid}</h1>;
-}
