c3c4d4374a778e4dad402291b7a59cf28c9cb27c
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-24 21:47:43 -0400

    lecture_485: home page display meetup list

---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import MeetupList from "../components/meetups/MeetupList";
+
+const DUMMY_MEETUPS = [
+    {
+        id: 1,
+        title: 'React Meetup',
+        address: '123 Main St',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Bangladesh_Bank_%2833398162476%29.jpg',
+        description: 'React Meetup Description'
+    },
+    {
+        id: 2,
+        title: 'Angular Meetup',
+        address: '123 Main St',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Bangladesh_Bank_%2833398162476%29.jpg',
+        description: 'Angular Meetup Description'
+    }
+]
+export default function HomePage() {
+    return <MeetupList meetups={DUMMY_MEETUPS}/>;
+}
