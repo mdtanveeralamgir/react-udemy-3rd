621d0ff04dee8aa85bf08afab6073ffb1b91f9b7
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-25 22:18:19 -0400

    lecture_489: component to pre rendering data

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,3 +1,10 @@
-export default function({ meetupId }) {
-    return <h1>{meetupId}</h1>
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+export default function MeetupDetail({meetupId}) {
+    return <MeetupDetails
+        image='https://upload.wikimedia.org/wikipedia/commons/5/5f/Bangladesh_Bank_%2833398162476%29.jpg'
+        title='A first meetup'
+        address='123 Main St'
+        description='A first meetup description'
+    />
 }
