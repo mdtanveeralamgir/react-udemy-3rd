ec7844acae52ba3c994e8d5060cf6868689c1cc0
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-28 22:15:19 -0400

    lecture_494: getStaticProps for dynamic page using param from query

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -8,3 +8,19 @@ export default function MeetupDetail({meetupId}) {
         description='A first meetup description'
     />
 }
+
+export async function getStaticProps(context) {
+    const meetupId = context.params.meetupId;
+    console.log(meetupId);
+    return {
+        props: {
+            meetupData: {
+                image: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Bangladesh_Bank_%2833398162476%29.jpg',
+                title: 'A first meetup',
+                address: '123 Main St',
+                description: 'A first meetup description',
+                id: meetupId
+            }
+        }
+    }
+}
