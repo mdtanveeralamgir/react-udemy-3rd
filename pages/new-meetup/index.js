5f8e161f23e6a017b4d7c6639c5c79bc1b4fbf45
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-29 21:51:30 -0400

    lecture_506: initial project

--- pages/new-meetup/index.js
+++
@@ -1,9 +0,0 @@
-import NewMeetupForm from "../../components/meetups/NewMeetupForm";
-
-export default function NewMeetup() {
-    function addMeetupHandler(enteredMeetupData) {
-        console.log(enteredMeetupData);
-    }
-
-    return <NewMeetupForm onAddMeetup={addMeetupHandler}/>;
-}
