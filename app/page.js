62e85d2196ba3ac1d8adc9f02baeaa5bd0265ae9
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-30 23:30:47 -0400

    lecture_512*: server action inside client component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,16 @@
 import RSCDemo from '../components/RSCDemo';
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/DataFetchingDemo";
+import ServerActionsDemo from "@/ServerActionsDemo";
 
 export default function Home() {
     return (
         <main>
             {/*using RSC to fetch data and return html element simplifies the fetching data*/}
             {/*this way it's not required to use react hook like useEffect*/}
-            <DataFetchingDemo/>
+            {/*<DataFetchingDemo/>*/}
+            <ServerActionsDemo/>
         </main>
+
     );
 }
