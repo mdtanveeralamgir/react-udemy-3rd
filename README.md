d77bc39098e165406925ca40497f9731760cf1ce
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-10-30 23:17:55 -0400

    lecture_511*: using RSC to fetch and display data without using react hooks

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 510*:
     - How to use RSC and client component inside one another
+511*:
+    - simplified way to fetch data and render without using react hooks
--- app/page.js
+++ app/page.js
@@ -1,13 +1,13 @@
 import RSCDemo from '../components/RSCDemo';
 import ClientDemo from "@/components/ClientDemo";
+import DataFetchingDemo from "@/DataFetchingDemo";
 
 export default function Home() {
     return (
         <main>
-
-            <ClientDemo>
-                <RSCDemo/>
-            </ClientDemo>
+            {/*using RSC to fetch data and return html element simplifies the fetching data*/}
+            {/*this way it's not required to use react hook like useEffect*/}
+            <DataFetchingDemo/>
         </main>
     );
 }
