5866f388644db59461e38c96e202f72e9fbd9ea6
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-11-12 22:13:42 -0500

    lecture_546: filter with input

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,8 +1,17 @@
+import {useState} from "react";
+
 export default function SearchableList({items}) {
+    const [searchTerm, setSearchTerm] = useState("");
+
+    function handleSearch(e) {
+        setSearchTerm(e.target.value)
+    }
+
+    const searchResults = items.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
     return <div className="searchable-list">
-        <input type="search" placeholder="Search"/>
+        <input type="search" placeholder="Search" onChange={handleSearch}/>
         <ul>
-            {items.map((item, index) => <li key={index}>{item.title}</li>)}
+            {searchResults.map((item, index) => <li key={index}>{item.title}</li>)}
         </ul>
     </div>
 }
