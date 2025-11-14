a468815e7a24a81a23fdc0f4c2d063acab61863d
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-11-13 22:22:34 -0500

    lecture_552: initial project

--- src/components/Accordion/AccordionTitle.jsx
+++
@@ -1,8 +0,0 @@
-import {useAccordianContext} from "./Accordion.jsx"
-import {useAccordionItemContext} from "./AccordionItem.jsx"
-
-export default function AccordionTitle({children}) {
-    const {toggleItem} = useAccordianContext();
-    const id = useAccordionItemContext();
-    return <h3 onClick={() => toggleItem(id)}>{children}</h3>
-}
