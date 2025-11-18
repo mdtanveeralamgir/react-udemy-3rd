4aa88a2fa5d232855e95f2962ad172200395a5d8
Md Tanveer Alamgir <mdtanveeralamgir@gmail.com>
2025-11-17 22:44:06 -0500

    lecture_572*: complex types, array, object and arrays of obj

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Commands:
     - compile a file: npx tsc <filename>
+572*: 
+    - complex types, array, object and arrays of obj
--- basics.ts
+++ basics.ts
@@ -2,3 +2,9 @@
 let age: number = 20;
 let name: string = "John";
 let isInstractor: boolean = false;
+//complex types
+let hobbies: string[] = ["music", "sports"];
+let person: {
+    name: string,
+    age: number
+}
